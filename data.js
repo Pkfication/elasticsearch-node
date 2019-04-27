02e82a0b0b53af9633b43f32d24cdbf36a0012a9
Pankaj Gupta <pankajgupta221b@gmail.com>
2019-04-27 18:31:38 +0530

    Updated config file, to hold the connections and modified for deleting an index as well

--- data.js
+++ data.js
@@ -1,22 +1,17 @@
-const es = require('elasticsearch');
 const cities = require('./cities.json');
-
-const client = new es.Client({
-  host: ['localhost:9200']
-});
+const client = require('./config').client;
 
 var bulk = [];
 
 cities.forEach(city =>{
   bulk.push({
     index:{ 
-      _index:"scotch.io-tutorial", 
+      _index:"cities", 
       _type:"cities_list",
     }          
   });
   bulk.push(city)
 });
-
 
 client.ping({
   requestTimeout:30000 
@@ -28,9 +23,8 @@ client.ping({
   }
 });
 
-// create a new index called scotch.io-tutorial. If the index has already been created, this function fails safely
 client.indices.create({
-      index: 'scotch.io-tutorial'
+      index: 'cities'
   }, function(error, response, status) {
       if (error) {
           console.log(error);
@@ -39,24 +33,21 @@ client.indices.create({
       }
 });
 
-// add a data to the index that has already been created
-client.index({
-     index: 'scotch.io-tutorial',
-     id: '1',
-     type: 'cities_list',
-     body: {
-         "Key1": "Content for key one",
-         "Key2": "Content for key two",
-         "key3": "Content for key three",
-     }
- }, function(err, resp, status) {
-     console.log(resp);
- });
+// For deleting an index
+// client.indices.delete({
+//   index: 'scotch.io-tutorial'
+// }, function(error, response, status) {
+//   if (error) {
+//       console.log(error);
+//   } else {
+//       console.log("DELETED the index", response);
+//   }
+// });
 
  client.bulk({ body: bulk }, function (err) { 
   if(err) { 
     console.log("Failed Bulk operation".red, err) 
   } else { 
-    console.log("Successfully imported %s".green, cities.length); 
+    console.log("Successfully imported", cities.length); 
   } 
 }); 
